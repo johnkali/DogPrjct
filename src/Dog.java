dd9d78ee7ed4072ed6ddab2f3a50b0df1d3c9194
John Kali <John Kali>
2016-08-16 15:29:50 +0300

    my second commit

---
+++ src/Dog.java
@@ -0,0 +1,42 @@
+/**
+class Dog {
+    
+        int size;
+        String breed;
+        String name;
+        String bark;
+    public void setName(String name) {
+		this.name = name;
+	}
+    public void setSize(int size) {
+		this.size = size;
+	}
+    public void setBark(String bark) {
+		this.bark = bark;
+	}    
+    public void bark() {
+        // TODO code application logic here
+        System.out.println("Hello, my name is " + name +" and i am size " + size + " and i " + bark+ "");
+        
+    }
+    
+
+        
+    
+    public static void main (String[] args) {
+        Dog d = new Dog();
+        d.setName("Jesse");
+        d.setBark("Ruff!! Ruff!!");
+        
+        
+        
+    
+        
+        
+    }
+}
+ */      
+      
+    
+    
+
